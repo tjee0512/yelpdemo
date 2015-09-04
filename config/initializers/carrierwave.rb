7c50bd0e4b56a5e63ab315a55f56bb1c39dd9351
Anthony Jee <anthonyjee0512@gmail.com>
2015-09-04 09:39:26 +0800

    added region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIJQJSRO3PGWKSJSA',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     ENV["aws_access_key_id"],                        # required
+    aws_secret_access_key: ENV["aws_secret_access_key"],                        # required
+    region: 'ap-southeast-2',
   }
-  config.fog_directory  = 'yelpdemo0512'                          # required
+  config.fog_directory  = ENV["fog_directory"]                          # required
 end
