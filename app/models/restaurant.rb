3c72d1ee02b91362b4686efc000b8e847aa3b9ae
Anthony Jee <anthonyjee0512@gmail.com>
2015-09-05 18:12:28 +0800

    Reset AWS key

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,6 +8,4 @@ class Restaurant < ActiveRecord::Base
     message: "must be in the format (123) 456-7890" }
   validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
     message: "must start with http:// or https://" }
-  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
-    message: "must be in the format 350 Fifth Avenue, New York, NY 10118" }
 end
